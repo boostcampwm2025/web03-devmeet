4206ce38b271d9fec8ead44b08b8cc3f63640ea9
seorang42 <seorang42@naver.com>
2025-12-18 04:02:47 +0900

    📍 Feat: 카드 UI 구현

--- frontend/src/types/workspace.ts
+++ frontend/src/types/workspace.ts
@@ -22,7 +22,7 @@ export interface BaseItem {
   scaleX: number;
   scaleY: number;
   opacity?: number;
-  zIndex?: number;
+  zIndex: number;
   isLocked?: boolean;
   isVisible?: boolean;
   name?: string;
@@ -44,6 +44,7 @@ export interface TextItem extends BaseItem {
   letterSpacing?: number;
   stroke?: string;
   strokeWidth?: number;
+  wrap?: string;
 }
 
 // 이미지

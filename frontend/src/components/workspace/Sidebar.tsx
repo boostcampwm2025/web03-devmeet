c541c218e3251757c3437183a7128f2bb302488e
tjsdn052 <tjsdn052@naver.com>
2025-12-18 19:03:22 +0900

    📍 Feat: sidebar 텍스트 버튼 클릭시 나타날 textpanel 작성
    - 동일 버튼 클릭시 탭 닫히도록 TabType null 추가
    
    - toggleTab 추가
    
    - 텍스트 패널 추가

--- frontend/src/components/workspace/Sidebar.tsx
+++ frontend/src/components/workspace/Sidebar.tsx
@@ -8,9 +8,9 @@ import { TextItem, ImageItem, VideoItem } from '@/types/Workspace';
 
 import NavButton from './sidebar/NavButton';
 import CardPanel from './sidebar/card/CardPanel';
-import ImagePanel from './sidebar/image/ImagePanel';
+import TextPanel from './sidebar/TextPanel';
 
-type TabType = 'card' | 'text' | 'image' | 'video';
+type TabType = 'card' | 'text' | 'image' | 'video' | null;
 
 export default function Sidebar() {
   const [activeTab, setActiveTab] = useState<TabType>('card');
@@ -42,6 +42,12 @@ export default function Sidebar() {
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [cardData]);
 
+  const toggleTab = (tab: TabType) => {
+    setActiveTab((prev) => (prev === tab ? null : tab));
+  };
+
+  // TODO : 각 아이템 추가 핸들러 구현
+
   return (
     <aside className="z-1 flex h-full border-r border-neutral-200 bg-white shadow-sm">
       {/* 좌측 네비게이션 */}
@@ -51,31 +57,28 @@ export default function Sidebar() {
             icon="/icons/sidebar/cardIcon.svg"
             label="카드"
             isActive={activeTab === 'card'}
-            onClick={() => setActiveTab('card')}
+            onClick={() => toggleTab('card')}
           />
           <NavButton
             icon="/icons/sidebar/textIcon.svg"
             label="텍스트"
             isActive={activeTab === 'text'}
-            onClick={() => {
-              setActiveTab('text');
+            onClick={() => toggleTab('text')}
+            onDoubleClick={() => {
+              handleAddText();
             }}
           />
           <NavButton
             icon="/icons/sidebar/imageIcon.svg"
             label="이미지"
             isActive={activeTab === 'image'}
-            onClick={() => {
-              setActiveTab('image');
-            }}
+            onClick={() => toggleTab('image')}
           />
           <NavButton
             icon="/icons/sidebar/videoIcon.svg"
             label="동영상"
             isActive={activeTab === 'video'}
-            onClick={() => {
-              setActiveTab('video');
-            }}
+            onClick={() => toggleTab('video')}
           />
         </div>
 
@@ -121,6 +124,7 @@ export default function Sidebar() {
 
       {/* 사이드바 상세 패널 */}
       {activeTab === 'card' && <CardPanel />}
+      {activeTab === 'text' && <TextPanel />}
       {/* TODO : 상세 패널 추가 */}
       {/* {activeTab === 'text' && <TextPanel />} */}
       {activeTab === 'image' && <ImagePanel />}

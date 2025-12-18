ec526be6add7ea1f37828b5885d4f802a262a48f
tjsdn052 <tjsdn052@naver.com>
2025-12-18 17:55:52 +0900

    🚚 Chore: 사용되지 않는 속성 제거

--- frontend/src/types/workspace.ts
+++ frontend/src/types/workspace.ts
@@ -40,10 +40,6 @@ export interface TextItem extends BaseItem {
   textDecoration: string;
   align: TextAlign;
   wrap: string;
-  lineHeight?: number;
-  letterSpacing?: number;
-  stroke?: string;
-  strokeWidth?: number;
 }
 
 // 이미지

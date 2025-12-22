669258f50f8ea8a896a0813a2559ea180b304c0c
KimDwDev <dongwan123456789@gmail.com>
2025-12-22 16:10:58 +0900

    📍 Feat: card_like에 대한 entity 구현

---
+++ rep/main_backend/src/1.domain/card/entities/card-like-entity.ts
@@ -0,0 +1,36 @@
+import { idVo } from "@domain/shared";
+import { cardIdVo, CardLikeProps } from "../vo";
+import { userIdVo } from "@domain/user/user.vo";
+
+
+export class CardLike {
+
+  private readonly id : CardLikeProps["id"];
+  private readonly card_id : CardLikeProps["card_id"];
+  private readonly user_id : CardLikeProps["user_id"];
+  private readonly created_at : Exclude<CardLikeProps["created_at"], undefined>;
+
+  constructor({
+    id, card_id, user_id, created_at = new Date()
+  } : CardLikeProps) {
+    this.id = idVo(id);
+    this.card_id = cardIdVo(card_id);
+    this.user_id = userIdVo(user_id);
+    this.created_at = created_at && created_at instanceof Date ? created_at : new Date();
+  };
+
+  getId() : CardLikeProps["id"] { return this.id; };
+  getCardId() : CardLikeProps["card_id"] { return this.card_id; };
+  getUserId() : CardLikeProps["user_id"] { return this.user_id; };
+  getCreatedAt() : Exclude<CardLikeProps["created_at"], undefined> { return this.created_at; };
+
+  getData() : Required<CardLikeProps> {
+    return {
+      id : this.id,
+      card_id : this.card_id,
+      user_id : this.user_id,
+      created_at : this.created_at
+    };
+  };
+
+};
--- rep/main_backend/src/1.domain/card/vo/index.ts
+++ rep/main_backend/src/1.domain/card/vo/index.ts
@@ -2,3 +2,4 @@ export * from "./card.vo";
 export * from "./card-item.vo";
 export * from './card-state.vo';
 export * from './category.vo';
+export * from "./card-like.vo";

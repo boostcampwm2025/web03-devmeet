57e008442cc90acdbfa3c92b3eb845cf0c6f5510
지태 <jitae@jitaeui-MacBookPro.local>
2026-01-08 02:15:44 +0900

    📍 Feat: arrow 폴더 분리

---
+++ frontend/src/assets/icons/whiteboard/arrow/index.ts
@@ -0,0 +1,4 @@
+// arrow icons
+export { default as ArrowIcon } from './arrowIcon.svg';
+export { default as DoubleArrowIcon } from './doubleArrowIcon.svg';
+export { default as ChevronArrowIcon } from './chevronArrowIcon.svg';

9676a9c1df6da5fb6cea2f6bb8af0709a1255820
chamny20 <dge3179@ajou.ac.kr>
2026-01-08 22:54:10 +0900

    🔧 Rename: 하나의 페이지 통일로 인한 MeetingRoom으로 컴포넌트 내용 이동

--- frontend/src/components/meeting/MeetingRoom.tsx
+++ frontend/src/components/meeting/MeetingRoom.tsx
@@ -1,8 +1,34 @@
+'use client';
+
+import ChatModal from '@/components/meeting/ChatModal';
+import InfoModal from '@/components/meeting/InfoModal';
+import MeetingMenu from '@/components/meeting/MeetingMenu';
+import MemberModal from '@/components/meeting/MemberModal';
+import MemberVideoBar from '@/components/meeting/MemberVideoBar';
+import { useMeeingStore } from '@/store/useMeetingStore';
+
 export default function MeetingRoom({ meetingId }: { meetingId: string }) {
+  const { isInfoOpen, isMemberOpen, isChatOpen } = useMeeingStore();
+
   return (
-    <div>
-      meeting room
-      <p className="mb-8 text-slate-400">회의 코드: {meetingId}</p>
-    </div>
+    <main className="flex h-screen w-screen flex-col bg-neutral-900">
+      <MemberVideoBar />
+
+      <section className="relative flex-1">
+        {/* 워크스페이스 / 코드 에디터 등의 컴포넌트가 들어갈 공간 */}
+
+        {/* 참가자 / 채팅창 */}
+        {(isMemberOpen || isChatOpen) && (
+          <div className="absolute top-2 right-2 bottom-2 flex w-80 flex-col gap-2">
+            {isMemberOpen && <MemberModal />}
+            {isChatOpen && <ChatModal />}
+          </div>
+        )}
+      </section>
+
+      <MeetingMenu />
+
+      {isInfoOpen && <InfoModal />}
+    </main>
   );
 }

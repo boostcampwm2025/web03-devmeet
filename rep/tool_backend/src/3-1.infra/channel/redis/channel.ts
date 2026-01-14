dd3cd1f2bc3e2664b5951ba09daf4774c7d31773
KimDwDev <dongwan123456789@gmail.com>
2026-01-15 06:27:25 +0900

    🚚 Chore: cache 모듈을 사용하기 위한 설정 완료
    - tool_backend에서 이제 REDIS_SERVER 심볼을 사용할 수 있습니다

--- rep/tool_backend/src/3-1.infra/channel/redis/channel.ts
+++ rep/tool_backend/src/3-1.infra/channel/redis/channel.ts
@@ -1,7 +1,8 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 
 
 // 나중에 sse가 필요하다면 그때 또 추가하는 방향도 있습니다.
+@Global()
 @Module({
 
 })
--- rep/tool_backend/src/app.module.ts
+++ rep/tool_backend/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
+import { RedisCacheModule } from '@infra/cache/redis/cache';
+import { RedisChannelModule } from '@infra/channel/redis/channel';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({}),
+
+    // infra 모듈
+    RedisCacheModule,
+    RedisChannelModule,
 
     // 추가 모듈
     HealthModule,

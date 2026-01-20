0ed4e790f8cdda077d7539a5b654bc6d51fb6c0b
chamny20 <dge3179@ajou.ac.kr>
2026-01-21 01:07:14 +0900

    📍 Feat: 코드에디터 소켓 훅 구현
    - 코드에디터 소켓 생성 로직 추가
    - 최초 툴 open로직 추가
    - 다른 유저 연결 요청 처리
    - 공유 중단 로직 추가

---
+++ frontend/src/hooks/useCodeEditorSocket.ts
@@ -0,0 +1,85 @@
+import { io } from 'socket.io-client';
+import { useMeetingSocketStore } from '@/store/useMeetingSocketStore';
+import { useToolSocketStore } from '@/store/useToolSocketStore';
+import { useMeetingStore } from '@/store/useMeetingStore';
+import { useCallback } from 'react';
+
+const TOOL_BACKEND_URL = process.env.NEXT_PUBLIC_TOOL_BACKEND_URL;
+const NAMESPACE = process.env.NEXT_PUBLIC_TOOL_BACKEND_WEBSOCKET_PREFIX;
+const SOCKET_PATH = process.env.NEXT_PUBLIC_TOOL_BACKEND_WEBSOCKET_CODEEDITOR;
+
+export const useCodeEditorSocket = () => {
+  const { socket: mainSocket } = useMeetingSocketStore(); // 시그널링 메인 소켓
+
+  const { setCodeEditorSocket } = useToolSocketStore();
+  const { setIsOpen } = useMeetingStore();
+
+  const connectToTool = useCallback(
+    (tool: string, ticket: string, type: 'main' | 'sub') => {
+      const newSocket = io(`${TOOL_BACKEND_URL}${NAMESPACE}`, {
+        path: SOCKET_PATH,
+        transports: ['websocket'],
+        auth: { token: ticket, type },
+      });
+
+      newSocket.on('connect', () => {
+        console.log(`${tool} 소켓 연결 성공 (타입: ${type})`);
+
+        if (tool === 'codeeditor') {
+          setCodeEditorSocket(newSocket);
+          setIsOpen('isCodeEditorOpen', true);
+        }
+      });
+
+      newSocket.on('disconnect', () => {
+        if (tool === 'codeeditor') setCodeEditorSocket(null);
+      });
+
+      return newSocket;
+    },
+    [setCodeEditorSocket, setIsOpen],
+  );
+
+  // 최초 툴 opener
+  const openCodeEditor = () => {
+    if (!mainSocket) return;
+
+    mainSocket.emit(
+      'signaling:ws:open_codeeditor',
+      (response: { ticket: string; tool: string }) => {
+        const { ticket, tool } = response;
+        connectToTool(tool, ticket, 'main');
+      },
+    );
+  };
+
+  // 다른 유저의 연결 요청 처리
+  const joinCodeEditor = useCallback(
+    (tool: string) => {
+      if (!mainSocket) return;
+
+      console.log(`${tool} 티켓 발급 요청 중...`);
+
+      mainSocket.emit(
+        'signaling:ws:connect_tool',
+        { tool },
+        (response: { ticket: string }) => {
+          if (response?.ticket) {
+            connectToTool(tool, response.ticket, 'sub');
+          }
+        },
+      );
+    },
+    [mainSocket, connectToTool],
+  );
+
+  // 공유 중단
+  const closeCodeEditor = () => {
+    if (!mainSocket) return;
+
+    mainSocket.emit('signaling:ws:disconnect_tool', { tool: 'codeeditor' });
+    setIsOpen('isCodeEditorOpen', false);
+  };
+
+  return { openCodeEditor, joinCodeEditor, closeCodeEditor };
+};
